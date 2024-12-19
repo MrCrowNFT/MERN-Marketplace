a5485cdb4671bde085912ca7bb7d46d24b37c600
MrCrowNFT <mrcrownft@gmail.com>
2024-12-19 16:36:38 -0300

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-//Entry pint for API
+//Entry point for API
 //Add "type": "module" to packege.json to use import
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("Server is ready");
-    res.end();
-})
+app.get("/products", (req, res) => {
+  res.send("Server is ready");
+});
 
 app.listen(5500, () => {
+  connectDB();
   console.log("Server started at http://localhost:5500/");
 });
