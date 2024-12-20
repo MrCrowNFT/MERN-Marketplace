f6ea047a1d54e37096d3824b55f5f38b71ffc6bd
MrCrowNFT <mrcrownft@gmail.com>
2024-12-19 23:24:41 -0300

    backend fix

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,17 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
-
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5500;
 
 app.use(express.json()); //To allow the app to accept JSON data in the req body
 
 //This gets completed in the router
 app.use("/api/products", productRoutes);
 
-app.listen(5500, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5500/");
+  console.log("Server started at http://localhost:" + PORT);
 });
