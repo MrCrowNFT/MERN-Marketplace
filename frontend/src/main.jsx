57f66f745ecdfe20e75c0afd8924733fc88ef2be
MrCrowNFT <mrcrownft@gmail.com>
2024-12-20 22:06:36 -0300

    app layout

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,18 @@
-import { Provider } from "@/components/ui/provider"
+//import chakra ui
+import { Provider } from "@/components/ui/provider";
+//import react router for handleling multiple pages
+import {BrowserRouter} from "react-router-dom";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
-    <App />
-    </Provider>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
